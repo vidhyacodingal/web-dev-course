91cc2faace9fc5925314173ab411012741778e7b
vidhyacodingal <vidhyacodingal@gmail.com>
2025-02-15 01:44:36 +0530

    commit

---
+++ lesson 36/script.js
@@ -0,0 +1,16 @@
+/*
+class Person {
+  constructor(name, job) {
+    this.name = name;
+    this.job = job;
+  }
+
+  print() {
+    const { name, job } = this;
+    console.log(`${name}, ${job}`);
+  }
+}
+
+const thatGuy = new Person('John Doe', 'Software Engineer');
+thatGuy.print();
+*/
